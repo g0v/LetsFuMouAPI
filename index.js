9e5f26e1a9b488b9f55e54b87512cf359e697e5d
Lee <jessy1092@gmail.com>
2014-03-28 13:57:23 +0800

    fix the bug. that remove the old loader and db.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 
 var config    = require('./lib/config');
-var loader    = require('./lib/loader');
-var dbClient  = require('./lib/db');
+// var loader    = require('./lib/loader');
+// var dbClient  = require('./lib/db');
 var app = express();
 
 var v = '1.0';
